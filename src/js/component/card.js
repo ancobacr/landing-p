9c72d894f69e1e5b42cc0297cadb7041adec5056
ancobacr <ancobacr@gmail.com>
2021-04-10 21:28:57 +0000

    updated data

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 export class Card extends React.Component {
-    render(){
-        <div className="col mb-4">
-            <div className="card h-100">
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-				sagittis euismod purus, sit amet pulvinar dolor porttitor sit
-				amet.</p>
-                </div>
-            </div>
-        </div>
-    }
+	render() {
+		<div className="col mb-4">
+			<div className="card h-100">
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Morbi sagittis euismod purus, sit amet pulvinar dolor
+						porttitor sit amet.
+					</p>
+				</div>
+			</div>
+		</div>;
+	}
 }
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,7 +9,7 @@ export function Navbar() {
 				</a>
 
 				<button
-					class="navbar-toggler"
+					className="navbar-toggler"
 					type="button"
 					data-toggle="collapse"
 					data-target="#navbarTogglerDemo02"
@@ -19,35 +19,33 @@ export function Navbar() {
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
-				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">
-								Home <span class="sr-only">(current)</span>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarTogglerDemo02">
+					<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
 								About
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
 								Services
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								Contact
+						<li className="nav-item">
+							<a className="nav-link" href="#"
+                             tabindex="-1"
+								aria-disabled="true">>
+								Contact  
 							</a>
 						</li>
-						<li class="nav-item">
-							<a
-								class="nav-link disabled"
-								href="#"
-								tabindex="-1"
-								aria-disabled="true"></a>
-						</li>
+						
 					</ul>
 				</div>
 			</div>
